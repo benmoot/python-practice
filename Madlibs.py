7e7751165082bb9f416c9047241709bb4820e5d9
benmoot <email.bennett.here@gmail.com>
2017-06-05 01:17:59 -0500

    TODO: fix Madlibs

---
+++ Madlibs.py
@@ -0,0 +1,24 @@
+#Input words; get funny story maybe.
+print "Welcome to MadLibs lil homie"
+name = raw_input("What is your name?")
+adj1 = raw_input("Give an adjective:")
+adj2 = raw_input("Give another adjective:")
+adj3 = raw_input("Give one more adjective:")
+ver1 = raw_input("Give a verb:")
+ver2 = raw_input("Give another verb:")
+ver3 = raw_input("Give one last verb:")
+nou1 = raw_input("Give a noun:")
+nou2 = raw_input("Give another noun:")
+nou3 = raw_input("Give a third noun:")
+nou4 = raw_input("Last noun I promise:")
+animal = raw_input("Name an animal:")
+food = raw_input("Name a food:")
+fruit = raw_input("Name a fruit:")
+number = raw_input("Give us a number:")
+superh = raw_input("Name a hero:")
+country = raw_input("What country are you from?:")
+dessert = raw_input("Your favorite dessert:")
+year = raw_input("What year is it?:")
+
+#The template for the story
+STORY = "This morning I woke up and felt %s because %s was going to finally %s over the big %s %s. On the other side of the %s were many %s protesting to keep %s in stores. The crowd began to %s to the rythym of the %s, which made all of the %s very %s. %s tried to %s into the sewers and found %s rats. Needing help, %s quickly called %s. %s appeared and saved %s by flying to %s and dropping %s into a puddle of %s. %s then fell asleep and woke up in the year %s, in a world where %s ruled the world." (name, adj1, adj3, animal)
