87096b29db30f8f23a48272d831614d179cdc52c
benmoot <email.bennett.here@gmail.com>
2017-06-12 17:25:28 -0500

    some more notes

--- string_methods.py
+++ string_methods.py
@@ -11,6 +11,10 @@ print "Norwegian Blue".lower()
 # prints string in all upper
 print "Norwegian Blue".upper()
 
+
+# methods with dot notation only work with strings
+
+
 # prints non-strings as strings
 # numbers are never strings unless made one
 pi = 3.14
@@ -20,3 +24,10 @@ print str(pi)
 # non-strings can be made strings with quotes
 pil = "3.144"
 print pil
+
+
+
+# Test
+print "The value of pi is around " + str(pi)
+
+# leave space after printed string for non-string to appear correctly
