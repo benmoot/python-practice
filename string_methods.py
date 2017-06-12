5fb84d8825b01c4d0086e73282f74ab4e164bbc3
benmoot <email.bennett.here@gmail.com>
2017-06-12 17:19:21 -0500

    String Methods

---
+++ string_methods.py
@@ -0,0 +1,22 @@
+# String Methods
+
+# prints length of string
+parrot = "Norwegian Blue"
+print len(parrot)
+
+
+# prints string in all lowercase
+print "Norwegian Blue".lower()
+
+# prints string in all upper
+print "Norwegian Blue".upper()
+
+# prints non-strings as strings
+# numbers are never strings unless made one
+pi = 3.14
+print str(pi)
+
+
+# non-strings can be made strings with quotes
+pil = "3.144"
+print pil
