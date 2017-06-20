42f400b645c8f593809f9a0f12fa092542d6121f
benmoot <email.bennett.here@gmail.com>
2017-06-20 00:47:23 -0500

    started cheatsheet

---
+++ cheatsheet.py
@@ -0,0 +1,43 @@
+# PYTHON CHEAT SHEET
+
+
+# import is used to make specialty functions available
+import random
+import sys
+import os
+
+# hello world is just one line of code
+# print () outputs data to the screen
+print ("Hello world")
+
+'''
+this is a mult-line comment
+'''
+
+# a variable is a place to store values
+name = "Derek"
+print(name)
+
+# var can contain letters, numbers or _ but not start with numbers
+# there are 5 data types:
+# numbers, strings, list, tuple, dictionary
+# you can store any of them in the same variable
+name = 15
+print (name)
+
+# arithmetic operators: +, -, *, /, %, **, //
+#  ** exponential calc
+# // floor division
+print("5+2 =", 5+2)
+print("5-2 =", 5-2)
+print("5*2 =", 5*2)
+print("5/2 =", 5/2)
+print("5%2 = ", 5%2)
+print("5 ** 2 =", 5**2)
+print("5 // 2 =", 5//2)
+
+# order of operations states * and / performed before + and -
+print("1 + 2 - 3 * 2 =", 1 + 2 - 3 * 2)
+print ("(1 + 2 - 3) *2 =", (1 + 2 - 3) * 2)
+
+# TODO: finish cheat sheet at: http://www.newthinktank.com/2014/11/python-programming/
